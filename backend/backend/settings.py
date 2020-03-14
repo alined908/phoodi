0dcd01804f03ae0b2ce145152fa2cdf3bb338a16
Daniel Lee <daniellee0228@berkeley.edu>
2020-03-14 14:44:20 -0700

    remove calendar

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,6 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            # "hosts": ['redis://localhost:6379/'],
             "hosts": [('redis', 6379)],
         },
     },
