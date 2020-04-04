fdb4acf74aea0ff3c6f80176435cfd75cd820e3c
Daniel Lee <daniellee0228@berkeley.edu>
2020-04-03 22:19:28 -0700

    update axiosClient

--- frontend/src/accounts/axiosClient.js
+++ frontend/src/accounts/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const axiosClient = axios.create({
     baseURL: 
-        (process.env.NODE_ENV === 'production') ? 
-        'https://phoodie.me':
-        `http://localhost:8000`
+        // (process.env.NODE_ENV === 'production') ? 
+        // 'https://phoodie.me':
+        `${process.env.REACT_APP_BASE_URL}`
 })
